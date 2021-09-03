64bd2a63b73a06d69e181eaf685b0324e64bd37f
Albina <axioma.25@gmail.com>
2021-09-03 23:31:02 +0500

    1

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/tests/'
-        : '/'
-    css: {
-        loaderOptions: {
-            scss: {
-                additionalData: `@import "@/assets/main.scss";`
-            }
-        }
-    }
+        : '/',
+    // css: {
+    //     loaderOptions: {
+    //         scss: {
+    //             additionalData: `@import "@/assets/main.scss";`
+    //         }
+    //     }
+    // }
 };
