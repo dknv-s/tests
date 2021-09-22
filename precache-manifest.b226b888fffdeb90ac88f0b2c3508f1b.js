ce7bf5e31359d457d5ffb71d3d6e9a56fdbb9eb2
Albina25 <77781085+Albina25@users.noreply.github.com>
2021-09-22 10:29:53 +0000

    Deploying to gh-pages from @ Albina25/tests@04727f97360a7f3b436e9ca5f676b8d1d38fe7c3 🚀

---
+++ precache-manifest.b226b888fffdeb90ac88f0b2c3508f1b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57c7681ca6aecfd1024",
+    "url": "/tests/css/app.30fd0fac.css"
+  },
+  {
+    "revision": "d8de52e6c5df1a987ef6b9126a70cfcc",
+    "url": "/tests/fonts/Nunito-Regular.d8de52e6.ttf"
+  },
+  {
+    "revision": "7d316b6786c13e4fc8bb0f67f1c96f4b",
+    "url": "/tests/fonts/Rubik-Italic.7d316b67.ttf"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/tests/fonts/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/tests/fonts/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/tests/fonts/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/tests/fonts/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/tests/fonts/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/tests/fonts/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/tests/fonts/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/tests/fonts/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/tests/fonts/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/tests/fonts/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/tests/fonts/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/tests/fonts/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "0dd5d5f73ff8f2f7686d65a99cb9172b",
+    "url": "/tests/icon-test.png"
+  },
+  {
+    "revision": "de41e9650abc7bef9a334e4cb7be3e9e",
+    "url": "/tests/icon-test1.png"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "/tests/img/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "/tests/img/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "/tests/img/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "2ae41cbc4a1feb9830c097cd94aadfac",
+    "url": "/tests/index.html"
+  },
+  {
+    "revision": "e57c7681ca6aecfd1024",
+    "url": "/tests/js/app.e27cd298.js"
+  },
+  {
+    "revision": "18475030ea4202b202f2",
+    "url": "/tests/js/chunk-vendors.b0dd2fbf.js"
+  },
+  {
+    "revision": "14b07612eddd8b3a83dbb0504e0a2f9d",
+    "url": "/tests/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tests/robots.txt"
+  }
+]);
